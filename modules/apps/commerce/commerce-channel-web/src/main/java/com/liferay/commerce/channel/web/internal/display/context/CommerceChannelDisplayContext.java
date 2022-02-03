3d5bcd2210b996d03cf3b4d82a16cbddf45e1f4a
Brian Chan <brian.chan@liferay.com>
2022-02-03 11:05:32 -0300

    COMMERCE-7311 Inline

--- modules/apps/commerce/commerce-channel-web/src/main/java/com/liferay/commerce/channel/web/internal/display/context/CommerceChannelDisplayContext.java
+++ modules/apps/commerce/commerce-channel-web/src/main/java/com/liferay/commerce/channel/web/internal/display/context/CommerceChannelDisplayContext.java
@@ -322,11 +322,10 @@ public class CommerceChannelDisplayContext
 			Collections.<ItemSelectorReturnType>singletonList(
 				new FileEntryItemSelectorReturnType()));
 
-		PortletURL itemSelectorURL = _itemSelector.getItemSelectorURL(
-			requestBackedPortletURLFactory, "addFileEntry",
-			fileItemSelectorCriterion);
-
-		return itemSelectorURL.toString();
+		return String.valueOf(
+			_itemSelector.getItemSelectorURL(
+				requestBackedPortletURLFactory, "addFileEntry",
+				fileItemSelectorCriterion));
 	}
 
 	@Override
